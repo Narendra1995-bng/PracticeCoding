fe619b45a4f1bd1f7f24d631216893affedfd308
Narendra Kumar Biswal <narendra.relyon@outlook.com>
2023-09-01 16:47:57 +0200

    Answer to combined result with Producttype defined

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,6 +15,8 @@ using System.Linq;
 //If prodcut is available in both list then ProductType is Modified
 //If product is not available in ProdcutB list means ProdcutType is Removed
 //If product was not there in ProdcutA but it is there in ProdcutB then ProcutType is New
+
+
 namespace PracticeCoding
 {
     public class Product
@@ -43,20 +45,20 @@ namespace PracticeCoding
         {
             List<Product> oldList = new List<Product>
             {
-                new Product { productId = 1, productName = "TVS", productDescription = "desc1 new" },
-                new Product { productId = 2, productName = "TVS1", productDescription = "desc2 new" },
-                new Product { productId = 3, productName = "TVS2", productDescription = "desc3 new" },
-                new Product { productId = 4, productName = "TVS3", productDescription = "desc4 new" },
-                new Product { productId = 5, productName = "TVS4", productDescription = "desc5 new" }
+                new Product { productId = 1, productName = "TVS", productDescription = "desc1" },
+                new Product { productId = 2, productName = "TVS1", productDescription = "desc2" },
+                new Product { productId = 3, productName = "TVS2", productDescription = "desc3" },
+                new Product { productId = 4, productName = "TVS3", productDescription = "desc4" },
+                new Product { productId = 5, productName = "TVS4", productDescription = "desc5" }
             };
 
             List<Product> newList = new List<Product>
             {
-                new Product { productId = 1, productName = "TVS", productDescription = "desc1 modified" },
-                new Product { productId = 2, productName = "TVS1", productDescription = "desc2 modified" },
-                new Product { productId = 7, productName = "TVS26", productDescription = "desc26 new" },
-                new Product { productId = 8, productName = "TVS33", productDescription = "TVS33 new" },
-                new Product { productId = 9, productName = "TVS42", productDescription = "TVS42 new" }
+                new Product { productId = 1, productName = "TVS", productDescription = "desc1" },
+                new Product { productId = 2, productName = "TVS1", productDescription = "desc2" },
+                new Product { productId = 7, productName = "TVS26", productDescription = "desc26" },
+                new Product { productId = 8, productName = "TVS33", productDescription = "TVS33" },
+                new Product { productId = 9, productName = "TVS42", productDescription = "TVS42" }
             };
 
             //1 - Common between the 2 list using binary search
@@ -101,6 +103,54 @@ namespace PracticeCoding
 
             // Now, 'subtractedList' contains the products that exist in 'newList' but not in 'oldList'.
 
+
+            // Prepare a new resultset with following condition
+            //Here you need to return List<ProductResult>
+            //which has data for all the products in both lists  i.e. ProductA and ProdcutB .
+            //If prodcut is available in both list then ProductType is Modified
+            //If product is not available in ProdcutB list means ProdcutType is Removed
+            //If product was not there in ProdcutA but it is there in ProdcutB then ProcutType is New
+
+            List<ProductResult> result = new List<ProductResult>();
+
+            foreach (var product in oldList)
+            {
+                var matchingProduct = newList.FirstOrDefault(p => p.productId == product.productId);
+
+                if (matchingProduct != null)
+                {
+                    result.Add(new ProductResult
+                    {
+                        Product = product,
+                        ProductType = ProductType.Modified
+                    });
+                }
+                else
+                {
+                    result.Add(new ProductResult
+                    {
+                        Product = product,
+                        ProductType = ProductType.Removed
+                    });
+                }
+            }
+
+            foreach (var product in newList)
+            {
+                var matchingProduct = oldList.FirstOrDefault(p => p.productId == product.productId);
+
+                if (matchingProduct == null)
+                {
+                    result.Add(new ProductResult
+                    {
+                        Product = product,
+                        ProductType = ProductType.New
+                    });
+                }
+            }
+            //end result
+
+
             //print values
 
             Console.WriteLine("Question1 ========= ");
@@ -121,7 +171,15 @@ namespace PracticeCoding
                 Console.WriteLine("--Id--" + item.productId + "--Name--" + item.productName + "--Desc--" + item.productDescription);
             }
 
-            Console.ReadKey();
+            Console.WriteLine("Question Combo ======= ");
+            foreach (var item in result)
+            {
+                Console.Write("--Id--" + item.Product.productId );
+                Console.Write("--Name--" + item.Product.productName);
+                Console.Write("--Desc--" + item.Product.productDescription);
+                Console.Write("--ProductType--" + item.ProductType);
+                Console.WriteLine();
+            }
         }
 
         public static int BinarySearch(List<Product> list, int productId)
