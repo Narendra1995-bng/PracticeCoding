a7dcb4463b640711488249ed3d583dc086e7809c
Narendra Kumar Biswal <narendra.relyon@outlook.com>
2023-09-03 13:43:52 +0200

    Some changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -61,49 +61,6 @@ namespace PracticeCoding
                 new Product { productId = 9, productName = "TVS42", productDescription = "desc42" }
             };
 
-            //1 - Common between the 2 list using binary search
-            oldList.Sort((x, y) => x.productId.CompareTo(y.productId));
-            newList.Sort((x, y) => x.productId.CompareTo(y.productId));
-
-            List<Product> commonProducts = new List<Product>();
-
-            foreach (var product in oldList)
-            {
-                if (BinarySearch(newList, product.productId) != -1)
-                {
-                    commonProducts.Add(product);
-                }
-            }
-
-            // Now, 'commonProducts' contains the common products between 'oldList' and 'newList'.
-
-            //2 - Substract newlist from oldlist
-            List<Product> subtractedList2 = new List<Product>();
-
-            foreach (var product in oldList)
-            {
-                if (!newList.Any(p => p.productId == product.productId))
-                {
-                    subtractedList2.Add(product);
-                }
-            }
-
-            // Now, 'subtractedList' contains the products that exist in 'oldList' but not in 'newList'.
-
-            //3 - Subtract oldlist from newlist
-            List<Product> subtractedList3 = new List<Product>();
-
-            foreach (var product in newList)
-            {
-                if (!oldList.Any(p => p.productId == product.productId))
-                {
-                    subtractedList3.Add(product);
-                }
-            }
-
-            // Now, 'subtractedList' contains the products that exist in 'newList' but not in 'oldList'.
-
-
             // Prepare a new resultset with following condition
             //Here you need to return List<ProductResult>
             //which has data for all the products in both lists  i.e. ProductA and ProdcutB .
@@ -111,6 +68,12 @@ namespace PracticeCoding
             //If product is not available in ProdcutB list means ProdcutType is Removed
             //If product was not there in ProdcutA but it is there in ProdcutB then ProcutType is New
 
+            List<ProductResult> productResultList = GetProductResult(oldList, newList);
+
+        }
+
+        public static List<ProductResult> GetProductResult(List<Product> oldList, List<Product> newList)
+        {
             List<ProductResult> result = new List<ProductResult>();
 
             foreach (var product in oldList)
@@ -148,38 +111,7 @@ namespace PracticeCoding
                     });
                 }
             }
-            //end result
-
-
-            //print values
-
-            Console.WriteLine("Question1 ========= ");
-            Console.WriteLine("Id\tName\tDesc");
-            foreach (var item in commonProducts)
-            {
-                Console.WriteLine(item.productId + "\t" + item.productName + "\t" + item.productDescription);
-            }
-
-            Console.WriteLine("Question2 ========= ");
-            Console.WriteLine("Id\tName\tDesc");
-            foreach (var item in subtractedList2)
-            {
-                Console.WriteLine(item.productId + "\t" + item.productName + "\t" + item.productDescription);
-            }
-
-            Console.WriteLine("Question3 ========= ");
-            Console.WriteLine("Id\tName\tDesc");
-            foreach (var item in subtractedList3)
-            {
-                Console.WriteLine(item.productId + "\t" + item.productName + "\t" + item.productDescription);
-            }
-
-            Console.WriteLine("Question Combo ======= ");
-            Console.WriteLine("Id\tName\tDesc\tProductType");
-            foreach (var item in result)
-            {
-                Console.WriteLine(item.Product.productId + "\t" + item.Product.productName + "\t" + item.Product.productDescription + "\t" + item.ProductType);
-            }
+            return result;
         }
 
         public static int BinarySearch(List<Product> list, int productId)
@@ -207,5 +139,9 @@ namespace PracticeCoding
 
             return -1; // Product not found.
         }
+
+        
     }
+
+    
 }
---
+++ ConsoleApp1/TestClass.cs
@@ -0,0 +1,88 @@
+﻿using PracticeCoding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCoding
+{
+    internal class TestClass
+    {
+
+
+
+        //1 - Common between the 2 list using binary search
+//        oldList.Sort((x, y) => x.productId.CompareTo(y.productId));
+//            newList.Sort((x, y) => x.productId.CompareTo(y.productId));
+
+//            List<Product> commonProducts = new List<Product>();
+
+//            foreach (var product in oldList)
+//            {
+//                if (BinarySearch(newList, product.productId) != -1)
+//                {
+//                    commonProducts.Add(product);
+//                }
+//}
+
+//        // Now, 'commonProducts' contains the common products between 'oldList' and 'newList'.
+
+//        //2 - Substract newlist from oldlist
+//        List<Product> subtractedList2 = new List<Product>();
+
+//            foreach (var product in oldList)
+//            {
+//                if (!newList.Any(p => p.productId == product.productId))
+//                {
+//                    subtractedList2.Add(product);
+//                }
+//}
+
+//// Now, 'subtractedList' contains the products that exist in 'oldList' but not in 'newList'.
+
+////3 - Subtract oldlist from newlist
+//List<Product> subtractedList3 = new List<Product>();
+
+//foreach (var product in newList)
+//{
+//    if (!oldList.Any(p => p.productId == product.productId))
+//    {
+//        subtractedList3.Add(product);
+//    }
+//}
+
+//            // Now, 'subtractedList' contains the products that exist in 'newList' but not in 'oldList'.
+
+
+        //print values
+
+//        Console.WriteLine("Question1 ========= ");
+//            Console.WriteLine("Id\tName\tDesc");
+//            foreach (var item in commonProducts)
+//            {
+//                Console.WriteLine(item.productId + "\t" + item.productName + "\t" + item.productDescription);
+//            }
+
+//    Console.WriteLine("Question2 ========= ");
+//            Console.WriteLine("Id\tName\tDesc");
+//            foreach (var item in subtractedList2)
+//            {
+//                Console.WriteLine(item.productId + "\t" + item.productName + "\t" + item.productDescription);
+//            }
+
+//Console.WriteLine("Question3 ========= ");
+//Console.WriteLine("Id\tName\tDesc");
+//foreach (var item in subtractedList3)
+//{
+//    Console.WriteLine(item.productId + "\t" + item.productName + "\t" + item.productDescription);
+//}
+
+//Console.WriteLine("Question Combo ======= ");
+//Console.WriteLine("Id\tName\tDesc\tProductType");
+//foreach (var item in result)
+//{
+//    Console.WriteLine(item.Product.productId + "\t" + item.Product.productName + "\t" + item.Product.productDescription + "\t" + item.ProductType);
+//}
+    }
+}
